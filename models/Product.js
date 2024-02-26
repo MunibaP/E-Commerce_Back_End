91eced40a147f2bcb2956bf97821df1bbc9ce4c9
MunibaP <munibapervez596@gmail.com>
2024-02-25 20:05:12 -0500

    Updated Product.js

--- models/Product.js
+++ models/Product.js
@@ -16,10 +16,12 @@ Product.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    // defining an 'product_name' columns
     product_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // defining an 'price' columns
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
@@ -27,6 +29,7 @@ Product.init(
         isDecimal: true,
       },
     },
+    // defining an 'stock' columns
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -35,6 +38,7 @@ Product.init(
         isNumeric: true,
       },
     },
+    // defining an 'category_id' columns
     category_id: {
       type: DataTypes.INTEGER,
       references: {

99432420d6838d8404cd9fa1ecfe0ad0ced82ac1
MunibaP <munibapervez596@gmail.com>
2024-02-25 21:46:06 -0500

    Added comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
+// Importing the Express Router module
 const router = require('express').Router();
+
+// Importing route modules for category, products, and tags.
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
+// Importing route handlers for categories, products, and tags endpoints.
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/tags', tagRoutes);
